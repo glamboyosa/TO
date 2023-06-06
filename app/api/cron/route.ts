ed682948a9742d55fdb1824bec41ee0ae6735f3d
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-06-06 12:26:29 +0100

    feat: add CRON

---
+++ app/api/cron/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server"
+
+import { freeCreditsExpiry } from "@/lib/helpers/freeCreditsExpiry"
+import { prismaClient } from "@/lib/prisma"
+
+export async function GET() {
+  const users = await prismaClient.user.findMany()
+  const usersList: Boolean[] = []
+  for (const user of users) {
+    const freeCreditsExpiryDate = freeCreditsExpiry()
+    const existingUserUpdated = await prismaClient.user.update({
+      where: {
+        email: user.email,
+      },
+      data: {
+        freeCreditsCreatedAt: new Date(),
+        freeCreditsExpiry: freeCreditsExpiryDate,
+      },
+      include: {
+        paidCredits: true,
+      },
+    })
+
+    existingUserUpdated ? usersList.concat(true) : usersList.concat(false)
+  }
+
+  if (usersList.every(Boolean) === true) {
+    NextResponse.json({ success: true })
+  } else {
+    NextResponse.json(
+      { success: false },
+      {
+        status: 400,
+      }
+    )
+  }
+}
